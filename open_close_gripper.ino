79c2836c4a8b6c8ed636d10a961b0b229bdc7ef2
Danny Staple <danny@orionrobots.co.uk>
2014-05-24 18:22:31 +0100

    Create open_close_gripper.ino

---
+++ open_close_gripper.ino
@@ -0,0 +1,29 @@
+/* Sweep uArm Gripper. Based on original Arduino sweep demo */
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+} 
+
+#define gripper_min 50
+#define gripper_max 90
+void loop() 
+{ 
+  for(pos = gripper_min; pos < gripper_max; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = gripper_max; pos>=gripper_min; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
